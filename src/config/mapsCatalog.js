4bdb0cb74cade7eea1371007d3556648eb1df015
GSC996 <gscoutinho@outlook.com>
2026-05-24 18:08:17 -0300

    fix(canvas): descargar mapas base en local para resolver problemas de CORS y carga

--- src/config/mapsCatalog.js
+++ src/config/mapsCatalog.js
@@ -2,19 +2,19 @@ export const mapsCatalog = [
   {
     id: 'buenos-aires',
     name: 'Provincia de Buenos Aires',
-    thumbnailUrl: 'https://mapasescolares.ign.gob.ar/images/mapas/MapaEscolar002_BsAs_2000x2500.png',
-    imageUrl: 'https://mapasescolares.ign.gob.ar/images/mapas/MapaEscolar002_BsAs_2000x2500.png',
+    thumbnailUrl: '/maps/MapaEscolar002_BsAs_2000x2500.png',
+    imageUrl: '/maps/MapaEscolar002_BsAs_2000x2500.png',
   },
   {
     id: 'cordoba',
     name: 'Provincia de Córdoba',
-    thumbnailUrl: 'https://mapasescolares.ign.gob.ar/images/mapas/MapaEscolar005_Cordoba_2000x2500.png',
-    imageUrl: 'https://mapasescolares.ign.gob.ar/images/mapas/MapaEscolar005_Cordoba_2000x2500.png',
+    thumbnailUrl: '/maps/MapaEscolar005_Cordoba_2000x2500.png',
+    imageUrl: '/maps/MapaEscolar005_Cordoba_2000x2500.png',
   },
   {
     id: 'santa-fe',
     name: 'Provincia de Santa Fe',
-    thumbnailUrl: 'https://mapasescolares.ign.gob.ar/images/mapas/MapaEscolar014_SantaFe_2000x2500.png',
-    imageUrl: 'https://mapasescolares.ign.gob.ar/images/mapas/MapaEscolar014_SantaFe_2000x2500.png',
+    thumbnailUrl: '/maps/MapaEscolar014_SantaFe_2000x2500.png',
+    imageUrl: '/maps/MapaEscolar014_SantaFe_2000x2500.png',
   },
 ]
