13846d3a141ab2b7416baa08b545db16295da5a8
GSC996 <gscoutinho@outlook.com>
2026-07-31 17:26:31 -0300

    fix: habilitar polling watch en vite.config y corregir ruta relativa de script en index.html para HMR

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/mapasescolares-v2/'
+  base: '/mapasescolares-v2/',
+  server: {
+    watch: {
+      usePolling: true
+    }
+  }
 })
